8dfbc8134cde78725184f1c912ba3eb9db0302d1
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-08-15 23:37:50 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,64 +1,64 @@
-RainbowSparkleUnicorn.start()
-let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
-let heart = strip.range(0, 12)
-let mouth = strip.range(12, 15)
-heart.setBrightness(50)
-mouth.setBrightness(50)
-heart.showColor(neopixel.colors(NeoPixelColors.Indigo))
-mouth.showColor(neopixel.colors(NeoPixelColors.Red))
-basic.showIcon(IconNames.Heart)
-mouth.showRainbow(1, 360)
-heart.showRainbow(1, 360)
-RainbowSparkleUnicorn.Sound.setVolume(10)
-RainbowSparkleUnicorn.Sound.playTrack(1)
+function startRobot() 
+{
+    RainbowSparkleUnicorn.start()
+    heart.setBrightness(50)
+    mouth.setBrightness(50)
+    heart.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    mouth.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.showIcon(IconNames.Heart)
+    mouth.showRainbow(1, 360)
+    heart.showRainbow(1, 360)
+    RainbowSparkleUnicorn.Sound.setVolume(10)
+    RainbowSparkleUnicorn.Sound.playTrack(1)
 
-basic.forever(function () {
-    basic.pause(1000);
-    mouth.rotate(1);
-    heart.rotate(1);
-    strip.show();
+    RainbowSparkleUnicorn.Light.turnAllOn();
+
+    basic.forever(function () {
+        basic.pause(1000);
+        mouth.rotate(1);
+        heart.rotate(1);
+        strip.show();
+    })
+}
+input.onButtonPressed(Button.A, function() {
+    RainbowSparkleUnicorn.Sound.setVolume(10)
+    RainbowSparkleUnicorn.Sound.playTrack(Math.randomRange(1,4))
 })
-
-function rainbowMouth() {
-    waiting2 = 200
+function rainbowMouth() 
+{
+  const  waiting = 200
     while (true) {
         drawMouth(1, NeoPixelColors.Red)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(2, NeoPixelColors.Yellow)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(3, NeoPixelColors.Green)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(4, NeoPixelColors.Blue)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(5, NeoPixelColors.Indigo)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(6, NeoPixelColors.Purple)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(7, NeoPixelColors.Orange)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(8, NeoPixelColors.White)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(7, NeoPixelColors.Red)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(6, NeoPixelColors.Yellow)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(5, NeoPixelColors.Green)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(4, NeoPixelColors.Blue)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(3, NeoPixelColors.Indigo)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(2, NeoPixelColors.Purple)
-        basic.pause(waiting2)
+        basic.pause(waiting)
         drawMouth(1, NeoPixelColors.Orange)
     }
 }
-let waiting2 = 0
-let LEDs = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
-heart.setBrightness(16)
-mouth.setBrightness(16)
-heart.showRainbow(1, 270)
-colourMouth(NeoPixelColors.Green);
 
 function colourMouth(color: NeoPixelColors) {
 
@@ -95,4 +95,8 @@ function drawMouth(size: number, color: NeoPixelColors) {
     mouth.show();
 }
 
+let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
+let heart = strip.range(0, 12)
+let mouth = strip.range(12, 15)
 
+startRobot();
