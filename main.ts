f0db3b963482421701b53b924118cfe63812f28c
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-08-23 22:24:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,103 +1,90 @@
-function startRobot() {
+// left button
+RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P0, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+
+function startRobot () {
     RainbowSparkleUnicorn.start()
     heart.setBrightness(50)
     mouth.setBrightness(50)
     heart.showColor(neopixel.colors(NeoPixelColors.Indigo))
-    //mouth.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.showIcon(IconNames.Square)
-    //mouth.showRainbow(1, 360)
     heart.showRainbow(1, 360)
-
     RainbowSparkleUnicorn.Sound.setVolume(13)
     RainbowSparkleUnicorn.Sound.playTrack(1)
-
-    RainbowSparkleUnicorn.Light.turnAllOn();
-
-    // colourMouth(neopixel.colors(NeoPixelColors.Purple))
-
-    basic.forever(function () {
+    RainbowSparkleUnicorn.Light.turnAllOn()
+    colourMouth(neopixel.colors(NeoPixelColors.Purple))
+basic.forever(function () {
         basic.pause(500);
-        // mouth.rotate(1);
         heart.rotate(1);
         strip.show();
     })
-
-    basic.forever(function () {
-        // basic.pause(500);      
-
-        let soundLevel = input.soundLevel();
-
-        if (soundLevel < 35) {
-            drawMouth(8, neopixel.colors(NeoPixelColors.Black));
-        } else {
-            drawMouth(8, neopixel.colors(NeoPixelColors.Purple));
-        }
-    })
-
-    radio.setGroup(76)
-    radio.setTransmitPower(7)
 }
-
-radio.onReceivedValue(function (name: string, value: number) {
-    basic.showString(name + ":" + value);
-})
-
 input.onButtonPressed(Button.A, function () {
-    RainbowSparkleUnicorn.Sound.setVolume(10)
-    RainbowSparkleUnicorn.Sound.playTrack(Math.randomRange(1, 4))
+    radio.sendString("VIBRATE")
 })
 
-function rainbowMouth() {
-    const waiting = 200
+radio.onReceivedString(function (receivedString) {
+	
+})
+
+// right button
+RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P3, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
+
+function rainbowMouth () {
+    waiting = 200
     while (true) {
         drawMouth(1, NeoPixelColors.Red)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(2, NeoPixelColors.Yellow)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(3, NeoPixelColors.Green)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(4, NeoPixelColors.Blue)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(5, NeoPixelColors.Indigo)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(6, NeoPixelColors.Purple)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(7, NeoPixelColors.Orange)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(8, NeoPixelColors.White)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(7, NeoPixelColors.Red)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(6, NeoPixelColors.Yellow)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(5, NeoPixelColors.Green)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(4, NeoPixelColors.Blue)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(3, NeoPixelColors.Indigo)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(2, NeoPixelColors.Purple)
-        basic.pause(waiting)
+basic.pause(waiting)
         drawMouth(1, NeoPixelColors.Orange)
     }
 }
-
+let waiting = 0
+let mouth: neopixel.Strip = null
+let heart: neopixel.Strip = null
 function colourMouth(color: NeoPixelColors) {
 
-    const waiting = 100;
+    const waiting2 = 100;
 
     while (true) {
         for (let i = 0; i <= 8; i++) {
             drawMouth(i, color)
-            basic.pause(waiting)
+            basic.pause(waiting2)
         }
         for (let j = 8; j > 1; j--) {
             drawMouth(j, color)
-            basic.pause(waiting)
+            basic.pause(waiting2)
         }
     }
 }
-
 function drawMouth(size: number, color: NeoPixelColors) {
 
     size = Math.constrain(size, 0, 8)
@@ -116,19 +103,9 @@ function drawMouth(size: number, color: NeoPixelColors) {
 
     mouth.show();
 }
-
 let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
-let heart = strip.range(0, 12)
-let mouth = strip.range(12, 15)
-
-startRobot();
-
-//left button
-RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P0, function () {
-    basic.showIcon(IconNames.Asleep)
-})
-
-//right button
-RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P3, function () {
-    basic.showIcon(IconNames.Tortoise)
-})
+heart = strip.range(0, 12)
+mouth = strip.range(12, 15)
+radio.setGroup(76)
+radio.setTransmitPower(7)
+startRobot()
