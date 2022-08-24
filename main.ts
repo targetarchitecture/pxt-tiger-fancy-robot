d88a5336c49af22e0b6eac5c0b8239f89148569b
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-08-25 00:48:00 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
-
-
-
-
+// left button
+RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P0, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+function startRobot () {
+    radio.setGroup(76)
+    radio.setTransmitPower(7)
+    heart.setBrightness(50)
+    heart.showRainbow(1, 360)
+    mouth.setBrightness(50)
+    mouth.showColor(neopixel.colors(NeoPixelColors.Red))
+    loops.everyInterval(500, function () {
+        heart.rotate(1);
+        strip.show();
+    })
+RainbowSparkleUnicorn.start()
+    RainbowSparkleUnicorn.Sound.setVolume(13)
+    RainbowSparkleUnicorn.Sound.playTrack(1)
+    RainbowSparkleUnicorn.Light.turnAllOn()
+}
 input.onButtonPressed(Button.A, function () {
     radio.sendString("VIBRATE")
 })
-
-radio.onReceivedString(function (receivedString) {
-    dealWithJoystickMessage(receivedString);
-})
-
-function dealWithJoystickMessage(receivedString: string) {
+function dealWithJoystickMessage (receivedString: string) {
     switch (receivedString) {
         case "BUTTON_LEFT":
             //
@@ -33,52 +44,24 @@ function dealWithJoystickMessage(receivedString: string) {
         case "THUMBSTICK_RIGHT":
             //
             break;
-        // case "THUMBSTICK_LEFT":
-        //     //
-        //     break;
-        // case "BUTTON_RIGHT":
-        //     //
-        //     break;
+        case "THUMBSTICK_LEFT":
+            //
+            break;
+        case "BUTTON_RIGHT":
+            //
+            break;
     }
 }
-
-function startRobot() {
-    radio.setGroup(76)
-    radio.setTransmitPower(7)
-
-    //heart.setBrightness(50)
-    //heart.showRainbow(1, 360)
-
-    //mouth.setBrightness(50)
-    //mouth.showColor(neopixel.colors(NeoPixelColors.Red))
-
-    // loops.everyInterval(500, function () {
-    //     heart.rotate(1);
-    //     strip.show();
-    // })
-
-    RainbowSparkleUnicorn.start()
-
-    RainbowSparkleUnicorn.Sound.setVolume(13)
-    RainbowSparkleUnicorn.Sound.playTrack(1)
-    RainbowSparkleUnicorn.Light.turnAllOn()
-
-}
-
-// left button
-RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P0, function () {
-    basic.showIcon(IconNames.Asleep)
+radio.onReceivedString(function (receivedString) {
+    dealWithJoystickMessage(receivedString)
 })
-
 // right button
 RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P3, function () {
     basic.showIcon(IconNames.Tortoise)
 })
-
-
-//let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
-//let heart = strip.range(0, 12)
-//let mouth = strip.range(12, 15)
-
-startRobot();
-
+let mouth: neopixel.Strip = null
+let heart: neopixel.Strip = null
+let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
+heart = strip.range(0, 12)
+mouth = strip.range(12, 15)
+startRobot()
