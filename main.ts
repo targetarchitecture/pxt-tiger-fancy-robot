a637a03363886a2b3757f8c89b8cd1233bb7fbfd
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-08-24 11:32:25 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
-
-
 function startRobot() {
+    radio.setGroup(76)
+    radio.setTransmitPower(7)
+
     RainbowSparkleUnicorn.start()
-    heart.setBrightness(50)
-    mouth.setBrightness(50)
-    heart.showColor(neopixel.colors(NeoPixelColors.Indigo))
-    mouth.showColor(neopixel.colors(NeoPixelColors.Red))
+    //heart.setBrightness(50)
+    //mouth.setBrightness(50)
+    //heart.showColor(neopixel.colors(NeoPixelColors.Indigo))
+   // mouth.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.showIcon(IconNames.Square)
-    heart.showRainbow(1, 360)
+   // heart.showRainbow(1, 360)
     RainbowSparkleUnicorn.Sound.setVolume(13)
     RainbowSparkleUnicorn.Sound.playTrack(1)
     RainbowSparkleUnicorn.Light.turnAllOn()
 
-    loops.everyInterval(500, function() {
-        heart.rotate(1);
-        strip.show();
-    })
+  //  loops.everyInterval(500, function () {
+    //    heart.rotate(1);
+     //   strip.show();
+   // })
 }
+
+startRobot()
+
 input.onButtonPressed(Button.A, function () {
     radio.sendString("VIBRATE")
 })
@@ -35,12 +39,8 @@ RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P3, func
     basic.showIcon(IconNames.Tortoise)
 })
 
-let mouth: neopixel.Strip = null
-let heart: neopixel.Strip = null
-
-let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
-heart = strip.range(0, 12)
-mouth = strip.range(12, 15)
-radio.setGroup(76)
-radio.setTransmitPower(7)
-startRobot()
+// let mouth: neopixel.Strip = null
+// let heart: neopixel.Strip = null
+//let strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB);
+//let heart = strip.range(0, 12);
+//let mouth = strip.range(12, 15);
