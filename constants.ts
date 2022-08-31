0236d56e1ccd5484788405c2c30b1bb59eeb9cd0
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-08-31 11:17:04 +0100

    Update pxt.json, main.ts, constants.ts

---
+++ constants.ts
@@ -0,0 +1,20 @@
+enum JoystickButtons {
+    //% block="THUMBSTICK LEFT"
+    THUMBSTICK_LEFT = 0,
+    //% block="THUMBSTICK RIGHT" 
+    THUMBSTICK_RIGHT = 1,
+    //% block="LEFT"
+    BUTTON_LEFT = 2,
+    //% block="RIGHT" 
+    BUTTON_RIGHT = 3,
+    //% block="BUTTON A"
+    BUTTON_A = 4,
+    //% block="BUTTON B" 
+    BUTTON_B = 5,
+    //% block="BUTTON AB"
+    BUTTON_AB = 6,
+}
+
+const headServo = RainbowSparkleUnicorn.Movement.Pins.P15;
+const leftArmServo = RainbowSparkleUnicorn.Movement.Pins.P14;
+const rightArmServo = RainbowSparkleUnicorn.Movement.Pins.P13;
--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 
-const headServo = RainbowSparkleUnicorn.Movement.Pins.P15;
-const leftArmServo = RainbowSparkleUnicorn.Movement.Pins.P14;
-const rightArmServo = RainbowSparkleUnicorn.Movement.Pins.P13;
-
 let LeftArmPWM = 339;
 let RightArmPWM = 240;
+let HeadPWM = 384;
+
+let joystickRX = 128;
+let joystickRY = 128;
+let joystickLX = 128;
+let joystickLY = 128;
+
+datalogger.log(null).
 
 RainbowSparkleUnicorn.Touch.onReleased(RainbowSparkleUnicorn.Touch.Pins.P3, function () {
     basic.showIcon(IconNames.Tortoise)
@@ -21,107 +25,92 @@ function startRobot() {
     heart.setBrightness(50)
     heart.showRainbow(1, 360)
     mouth.setBrightness(50)
-    mouth.showColor(neopixel.colors(NeoPixelColors.Blue))
+    mouth.showColor(neopixel.colors(NeoPixelColors.Indigo))
+
+    RainbowSparkleUnicorn.Sound.setVolume(15)
+    RainbowSparkleUnicorn.Sound.playTrack(1)
+    RainbowSparkleUnicorn.Light.turnAllOn()
+
+    RainbowSparkleUnicorn.Movement.setServoPulse(headServo, HeadPWM);
+    RainbowSparkleUnicorn.Movement.setServoPulse(leftArmServo, LeftArmPWM);
+    RainbowSparkleUnicorn.Movement.setServoPulse(rightArmServo, RightArmPWM);
+
+    //loops
     loops.everyInterval(500, function () {
         heart.rotate(1);
         strip.show();
     })
 
-    RainbowSparkleUnicorn.Sound.setVolume(13)
-    RainbowSparkleUnicorn.Sound.playTrack(1)
-    RainbowSparkleUnicorn.Light.turnAllOn()
-
-    RainbowSparkleUnicorn.Movement.setServoPulse(headServo, 384);
-    RainbowSparkleUnicorn.Movement.setServoPulse(leftArmServo, LeftArmPWM);
-    RainbowSparkleUnicorn.Movement.setServoPulse(rightArmServo, RightArmPWM);
-
+    loops.everyInterval(100, function () {
+        dealWithServoMovements();
+    })
 }
+
 input.onButtonPressed(Button.A, function () {
     radio.sendString("VIBRATE")
 })
 
-function dealWithJoystickMessage(receivedString: string) {
-    switch (receivedString) {
-        case "BUTTON_LEFT":
-            //     RainbowSparkleUnicorn.Movement.setServoPulse(RainbowSparkleUnicorn.Movement.Pins.P3,123);
-            //
-            break;
-        case "TILTRIGHT":
-            //
+function dealWithServoMovements() {
 
-            break;
-        case "BUTTON_B":
-            radio.sendString("VIBRATE");
-            break;
-        case "TILTLEFT":
-            //
-            break;
-        case "BUTTON_AB":
-            //
-            break;
-        case "BUTTON_A":
-            //
-            break;
-        case "THUMBSTICK_RIGHT":
-            //
-            break;
-        case "THUMBSTICK_LEFT":
-            //
-            break;
-        case "BUTTON_RIGHT":
-            //
-            break;
-        default:
+    if (joystickRY > 128 + 8) {
+        RightArmPWM = RightArmPWM + 1;
+    } else if (joystickRY < 120) {
+        RightArmPWM = RightArmPWM - 1;
+    }
+    //{
+    //     const pwm = Math.round(Math.map(RY, 0, 256, 426 - 240, 426));
+    //     RainbowSparkleUnicorn.Movement.setServoPulse(rightArmServo, pwm);
+    // }
 
-            const parts = receivedString.split(",")
-            const X = parts[0].substr(3);
-            const Y = parts[1].substr(3);
+    led.plotBarGraph(joystickRY, 255)
+    RainbowSparkleUnicorn.Movement.setServoPulse(rightArmServo, RightArmPWM);
+    RainbowSparkleUnicorn.Movement.setServoPulse(leftArmServo, RightArmPWM);
+    //}
 
-            if (receivedString.substr(0, 2) == "RX") {
-                const RX = parseInt(X);
-                const RY = parseInt(Y);
 
-                // if (RY > 120 || RY < 128 + 8) {
-                //     RightArmPWM= 339;
-                // } else
-                if (RY > 128 + 8) {
-                    RightArmPWM = RightArmPWM + 1;
-                } else if (RY < 120) {
-                    RightArmPWM = RightArmPWM - 1;
-                }
-                //{
-                //     const pwm = Math.round(Math.map(RY, 0, 256, 426 - 240, 426));
-                //     RainbowSparkleUnicorn.Movement.setServoPulse(rightArmServo, pwm);
-                // }
+    const pwm = Math.round(Math.map(joystickLX, 0, 255, 500, 278));
+    RainbowSparkleUnicorn.Movement.setServoPulse(headServo, pwm);
+}
 
-                led.plotBarGraph(RightArmPWM, 256)
-                RainbowSparkleUnicorn.Movement.setServoPulse(rightArmServo, RightArmPWM);
-            }
+function dealWithJoystickButton(value: JoystickButtons) {
 
-            if (receivedString.substr(0, 2) == "LX") {
-                const LX = parseInt(X);
-                const LY = parseInt(Y);
-
-                if (LY > 120 || LY < 128 + 8) {
-                    RainbowSparkleUnicorn.Movement.setServoPulse(leftArmServo, 339);
-                } else {
-
-                    const pwm = Math.round(Math.map(LY, 0, 256, 339 - 155, 155));
-
-                    RainbowSparkleUnicorn.Movement.setServoPulse(leftArmServo, pwm);
-                }
-            }
-
+    switch (value) {
+        case JoystickButtons.BUTTON_B:
+            radio.sendValue("VIBRATE", 500);
             break;
     }
 }
 
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedValue(function (name: string, value: number) {
 
-    dealWithJoystickMessage(receivedString);
 
-    led.toggle(0, 0);
+    switch (name) {
+        case "PRESSED":
+            dealWithJoystickButton(value);
+            //
+            break;
+        case "GESTURE":
+            //
+            break;
+        case "RX":
+            joystickRX = value;
+            //
+            break;
+        case "RY":
+            joystickRY = value;
+            //
+            break;
+        case "LX":
+            joystickLX = value;
+            //
+            break;
+        case "LY":
+            //
+            joystickLY = value;
+            break;
+    }
 })
+
 
 let mouth: neopixel.Strip = null
 let heart: neopixel.Strip = null
